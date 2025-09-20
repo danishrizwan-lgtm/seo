87a9a82527096310ba9a39e3c127b39cc02672ee
Manasa Rayi <manasa@tektreeinc.com>
2025-09-20 17:03:34 +0530

    Initial commit - Dummy SEO appMMfff

--- lib/dummyData.ts
+++ lib/dummyData.ts
@@ -15,7 +15,8 @@ export const posts = [
     excerpt:
       "Learn how to add SEO to your Next.js app with metadata and sitemap.",
     content: "This is the full post content...",
-    postimage: "/og-image.jpg",
+    postimage:
+      "https://prime9staging.s3.ap-south-1.amazonaws.com/prime9/images/12364544/posts/09_2025/Supreme_Court_1758350345721.jpg",
     updatedAt: "2025-09-20",
   },
 ];
